bd6f3d6230ea787a8dace1aaa67e55404c1204f3
vulgoguix <gpeuvis@gmail.com>
2022-06-23 09:31:01 -0300

    add: fix

--- cypress/integration/get.spec.js
+++ cypress/integration/get.spec.js
@@ -2,7 +2,7 @@
 
 describe('GET /characters', function() {
     it('deve retornar uma lista de personagens', function() {
-            cy.getCharacters.then(function(response) {
+            cy.getCharacters().then(function(response) {
                 expect(response.status.to.equal(200))
             })
     })
